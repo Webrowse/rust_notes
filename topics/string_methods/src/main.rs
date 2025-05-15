56ae30e7bdc1619a608b17f49b58dda036a30970
Webrowse <great.adarsh@gmail.com>
2025-05-16 05:20:17 +0530

    string completed 10 exercises

--- topics/string_methods/src/main.rs
+++ topics/string_methods/src/main.rs
@@ -32,14 +32,63 @@
 //     }
     
     // Exercise
+fn main(){
 
-// 1. Create a `String`, push multiple characters and words, print final value.
-// 2. Check `len` and `is_empty` before and after modifying the string.
-// 3. Use `contains` with various substrings to test pattern matching.
-// 4. Replace different words inside the string and print each result.
-// 5. Split a sentence into words using `split_whitespace`, print each.
-// 6. Trim a string with mixed whitespace, confirm leading/trailing removal.
-// 7. Convert a lowercase string with padding to uppercase and print it.
-// 8. Attempt direct indexing like `s[0]`, observe and explain compiler error.
-// 9. Chain methods: trim → to\_uppercase → replace → collect into `Vec<_>`.
-// 10. Compare `String` and `&str` methods—build a table of method availability.
+    // 1. Create a `String`, push multiple characters and words, print final value.
+    let mut str1 = String::from("hello");
+    str1.push(' ');
+    str1.push(':');
+    str1.push_str(" yeah");
+    println!("1. str1: {}",str1);
+
+    // 2. Check `len` and `is_empty` before and after modifying the string.
+    let mut str2 = String::new();
+    println!("2. len before: {}",str2.len());
+    println!("   is_empty before: {}",str2.is_empty());
+    str2.push_str("Apple");
+    println!("   len after: {}",str2.len());
+    println!("   is_empty after: {}",str2.is_empty());
+    // 3. Use `contains` with various substrings to test pattern matching.
+    println!("3. str2 contains 'ban'?: {}",str2.contains("ban"));
+    println!("   str2 conatins 'pp'?: {}",str2.contains("pp"));
+    // 4. Replace different words inside the string and print each result.
+    let str4 = String::from("hello rust");
+    println!("4. str4 before replace(): {}",str4);
+    let replaced = str4.replace("rust","crusty");
+    println!("   str4 after replace(): {}",replaced);
+    // 5. Split a sentence into words using `split_whitespace`, print each.
+    let str5 = String::from("hello hi i'm learning rust");
+    println!("5. split_whitespace: ");
+    for str in str5.split_whitespace(){
+        println!("    {}",str);
+    }
+    // 6. Trim a string with mixed whitespace, confirm leading/trailing removal.
+    let str6 = String::from("   hi  yess    ");
+    println!("6. untrimmed: {}",str6);
+    println!("   trimmed: {}",str6.trim());
+    // 7. Convert a lowercase string with padding to uppercase and print it.
+    println!("7. UPPERCASING str6: {}",str6.to_uppercase().trim());
+    // 8. Attempt direct indexing like `s[0]`, observe and explain compiler error.
+    // println!("8. direct indexing: {}", str6.trim().[0]);
+    // 9. Chain methods: trim → to uppercase → replace → collect into `Vec<_>`.
+    let s9 = String::from("       lowercase text    ");
+    let uped_s9: String = s9.trim().replace("lowercase","uppercase").to_uppercase();
+    let vec_s9: Vec<_> = uped_s9.split_whitespace().collect();
+    println!("{:?}",vec_s9);
+    // 10. Compare `String` and `&str` methods—build a table of method availability.
+    let cs = "  hello  rust  world";
+    let ct = cs.trim();
+    println!("{}",ct); //Hence, trim() works both on &str and String.
+     //Hence, push() do not works both on &str but works on String.
+    println!("{}",cs.len()); //len() works on both &str and String
+    println!("{}",cs.is_empty()); //is_empty() works on both &str and String
+
+    println!("{}",cs.contains("el")); //contains() works on both &str and String
+    println!("{}",cs.replace("el","le")); // replace() works on both &str and String
+
+    for s in cs.split_whitespace(){
+        println!("{}",s);
+    } // spilt_whitespace works on both &str and String
+
+    //indexing is not allowed in both &str and string.
+}
