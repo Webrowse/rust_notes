c561c072460215f9c5cfc1c02a2e4804abb42be9
Webrowse <great.adarsh@gmail.com>
2025-06-09 06:58:31 +0530

    crates 10 exercises done: Before review

--- topics/crates_and_dependencies/src/main.rs
+++ topics/crates_and_dependencies/src/main.rs
@@ -5,78 +5,53 @@
 // or CLI command: cargo add fastrand
 
 use fastrand;
-use std::{cmp::max, collections::HashMap};
+use std::{cmp::max, collections::HashMap, thread::yield_now, time::Instant};
 
 fn _generate_random_number() -> i32 {
     // Generates a number in range [1, 100]
     fastrand::i32(1..=100)
 }
 
-fn main() {
-    /*
-
-       let random = generate_random_number();
-       println!("Random number: {}", random);
-
-       // Generate 5 random numbers and find the max
-       let nums: Vec<i32> = (0..5).map(|_| fastrand::i32(1..=100)).collect();
-       println!("Generated numbers: {:?}", nums);
-
-       if let Some(max) = nums.iter().max() {
-        println!("Maximum value: {}", max);
-    }
-    */
-    println!("Ex 1: \n\t{}", fastrand_function());
-    println!("Ex 2: \n\t{:?}", print_10_random());
-    let (a, b, c) = count_pos_neg();
-    println!("Ex 3: \n\tPositive: {}, Negative: {}, Zero: {}", a, b, c);
-    println!("Ex 4: \n\t{}", generate_ranged(500..=1000));
-    println!("Ex 5: \n\t{}", average_100_random());
-    let (a, b) = coin_toss();
-    println!("Ex 6: \n\tCount of True: {},\n\tCount of False: {}", a, b);
-    let (a, b) = count_numbers();
-    println!("Ex 7: \n\tNumber {} appears {} times", a, b);
-}
 
 // 1. Create a binary project using Cargo. Add `fastrand` as a dependency. Verify it builds without error.
-fn fastrand_function() -> i32 {
+fn _fastrand_function() -> i32 {
     fastrand::i32(0..=50)
 }
 // 2. Write a function that returns a random `u8` between 0 and 255 using `fastrand::u8`. Print 10 outputs.
-fn print_10_random() -> Vec<u8> {
+fn _print_10_random() -> Vec<u8> {
     (0..10).map(|_| fastrand::u8(0..=255)).collect::<Vec<u8>>()
 }
 // 3. Generate a `Vec<i32>` of 20 random integers in the range -50 to 50. Count how many are positive, negative, or zero.
-fn count_pos_neg() -> (usize, usize, usize) {
+fn _count_pos_neg() -> (usize, usize, usize) {
     let random20 = (0..20)
-        .map(|_| fastrand::i32(-50..=50))
-        .collect::<Vec<i32>>();
-    let mut pos = Vec::new();
-    let mut neg = Vec::new();
-    let mut zero = Vec::new();
-    for i in random20.iter() {
-        match *i {
-            x if x > 0 => pos.push(i),
-            x if x < 0 => neg.push(i),
-            x if x == 0 => zero.push(i),
-            _ => unreachable!(),
-        }
+    .map(|_| fastrand::i32(-50..=50))
+    .collect::<Vec<i32>>();
+let mut pos = Vec::new();
+let mut neg = Vec::new();
+let mut zero = Vec::new();
+for i in random20.iter() {
+    match *i {
+        x if x > 0 => pos.push(i),
+        x if x < 0 => neg.push(i),
+        x if x == 0 => zero.push(i),
+        _ => unreachable!(),
     }
-    (pos.len(), neg.len(), zero.len())
+}
+(pos.len(), neg.len(), zero.len())
 }
 // 4. Modify the original `generate_random_number` function to accept a range as parameters. Use it to generate numbers in range 500 to 1000.
-fn generate_ranged(range: std::ops::RangeInclusive<i32>) -> i32 {
+fn _generate_ranged(range: std::ops::RangeInclusive<i32>) -> i32 {
     fastrand::i32(range)
 }
 // 5. Write a function that returns the average of 100 random `i32` values in the range 1 to 10.
-fn average_100_random() -> i32 {
+fn _average_100_random() -> i32 {
     let arr = (0..100)
-        .map(|_| fastrand::i32(1..=10))
-        .collect::<Vec<i32>>();
-    return (arr.iter().sum::<i32>()) / 100;
+    .map(|_| fastrand::i32(1..=10))
+    .collect::<Vec<i32>>();
+return (arr.iter().sum::<i32>()) / 100;
 }
 // 6. Use `fastrand::bool()` to simulate 100 coin tosses. Print the count of heads vs tails.
-fn coin_toss() -> (usize, usize) {
+fn _coin_toss() -> (usize, usize) {
     let mut true_count: usize = 0;
     let mut false_count: usize = 0;
     let all_toss: Vec<bool> = (0..100).map(|_| fastrand::bool()).collect();
@@ -90,24 +65,83 @@ fn coin_toss() -> (usize, usize) {
     (true_count, false_count)
 }
 // 7. Generate 1000 random numbers between 1 and 100. Store the frequency count in a `HashMap<i32, usize>`. Print the most frequent number.
-fn count_numbers() -> (i32, usize) {
+fn _count_numbers() -> (i32, usize) {
     let all_random: Vec<i32> = (0..1000).map(|_| fastrand::i32(1..=100)).collect();
     let mut all_numbers: HashMap<i32, usize> = HashMap::new();
     let mut max_freq = 0;
-
+    
     for &num in all_random.iter() {
         let count = all_numbers.entry(num).or_insert(0);
         *count += 1;
         max_freq = max(max_freq, *count);
     }
-    let (&most_number, _most_freq) = all_numbers.iter()
-                                                .find(|&(_key, &val)| val == max_freq)
-                                                .expect("we have the value for sure");
- 
-    (most_number, max_freq)
+    let (&most_number, &most_freq) = all_numbers.iter()
+    .find(|&(&_key, &val)| val == max_freq)
+    .expect("we have the value for sure");
+
+    (most_number, most_freq)
 }
-// 8. Implement a function that returns a random element from a slice. Use it to randomly choose a string from an array of 5 names.
+// 8. Implement a function that returns a random element from a slice. 
+// Use it to randomly choose a string from an array of 5 names.
+fn _random_element(arg: &[String]) -> String{
+    let length_of_arg = (arg.len()) as i32;
+    let r_num = (fastrand::i32(0..length_of_arg)) as usize;
+    let r_chose = arg[*&r_num].clone();
+    r_chose
+}
+// 9. Benchmark the runtime of generating 1 million random numbers using `std::time::Instant`. 
+// Print elapsed time.
+fn _benching()-> u128{
+    let initial = Instant::now();
+    let _ = (0..1_000_000).map(|_|fastrand::i32(0..100)).collect::<Vec<i32>>();
+    let finally = Instant::now();
+    (finally-initial).as_millis()
+    
+}
+// 10. Without using `.max()`, implement logic to find the largest value in a vector of random numbers. 
+// Write this in a separate function.
+fn find_max()->i32{
+    let random_vec = (0..100).map(|_|fastrand::i32(0..100)).collect::<Vec<i32>>();
+    let mut max_num = 0;
+    for i in random_vec.iter(){
+        if max_num < *i {
+            max_num = *i;
+        }
+    };
+    max_num
+    
+}
+fn run(){
+ println!("Max random without Max(): {}.",find_max());
+}
+fn main() {
+    run();
 
-// 9. Benchmark the runtime of generating 1 million random numbers using `std::time::Instant`. Print elapsed time.
+    /*
 
-// 10. Without using `.max()`, implement logic to find the largest value in a vector of random numbers. Write this in a separate function.
+       let random = generate_random_number();
+       println!("Random number: {}", random);
+
+       // Generate 5 random numbers and find the max
+       let nums: Vec<i32> = (0..5).map(|_| fastrand::i32(1..=100)).collect();
+       println!("Generated numbers: {:?}", nums);
+
+       if let Some(max) = nums.iter().max() {
+        println!("Maximum value: {}", max);
+    }
+    */
+    /*
+    println!("Ex 1: \n\t{}", fastrand_function());
+    println!("Ex 2: \n\t{:?}", print_10_random());
+    let (a, b, c) = count_pos_neg();
+    println!("Ex 3: \n\tPositive: {}, Negative: {}, Zero: {}", a, b, c);
+    println!("Ex 4: \n\t{}", generate_ranged(500..=1000));
+    println!("Ex 5: \n\t{}", average_100_random());
+    let (a, b) = coin_toss();
+    println!("Ex 6: \n\tCount of True: {},\n\tCount of False: {}", a, b);
+    let (a, b) = count_numbers();
+    println!("Ex 7: \n\tNumber {} appears {} times", a, b);
+    let s = ["Adarsh".to_string(),"Romy".to_string(),"Akshay".to_string()];
+    println!("Exercise 8: \n\tRandomly chosen out of Adarsh, Romy, Akshay: {}",random_element(&s));
+    */
+}
