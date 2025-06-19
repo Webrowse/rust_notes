ddf66705654736253f69a5685db5d3dfcc5a6478
Webrowse <great.adarsh@gmail.com>
2025-06-20 00:48:46 +0530

    2 serde exercises done

--- topics/code_tesing_playground/src/main.rs
+++
@@ -1,6 +0,0 @@
-// Exercise 2: Field Renaming and Validation
-// Make a struct Person with #[serde(rename = "firstName")] name: String.
-//     Serialize an instance.
-//     Manually write JSON with incorrect key (e.g., name instead of firstName).
-//     Attempt deserialization. Catch and print the error.
-
