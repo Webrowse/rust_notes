6126b38ada49e468a790cbcbebb415fc87f7a6c6
Webrowse <great.adarsh@gmail.com>
2025-06-25 23:52:07 +0530

    Solved 1st clap exercise and listed 2nd problem

--- topics/code_tesing_playground/src/main.rs
+++ topics/code_tesing_playground/src/main.rs
@@ -1,26 +1,7 @@
-// 1. Arg Count Behavior
-// Run with:cargo run -- -vvv  
+// 2. Single Arg Extraction
+// Run:
 
-// Confirm output: Verbosity level: 3
-// Then remove ArgAction::Count, observe behavior. 
-// Understand count vs boolean action.
+// cargo run -- --name adarsh  
 
-use clap::{Arg, Command};
-
-fn main(){
-    let matches = Command::new("cli-tool")
-    .author("Romy")
-    .arg(
-        Arg::new("verbose")
-            .short('v')
-    )
-    .get_matches();
-
-    let level = matches.get_count("verbose");
-    println!("Verbosity level: {}", level);
-    
-
-    if matches.get_flag("verbose"){
-        println!("Verbose flag is present");
-    }
-}
+// Confirm Name: adarsh prints.
+// Then remove .num_args(1), run again, and analyze result.
